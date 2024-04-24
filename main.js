215ea383839eb00e98601d1735463e66de90422b
BryanGarciaJmz <bjgj6318@gmail.com>
2024-04-24 10:22:20 -0600

    Add logic and a clear button

--- main.js
+++ main.js
@@ -5,14 +5,33 @@ function addRows() {
         const row = document.createElement('div');
         row.classList.add('row');
         container.appendChild(row);
-
+        
         for (let i = 0; i <= 15; i++) {
             const grid = document.createElement('div');
             grid.classList.add('box');
             row.appendChild(grid);
         }
-
     }
 }
 
 addRows();
+
+const boxes = document.querySelectorAll('.box');
+
+boxes.forEach((box) => {
+    box.addEventListener("mouseover", () => {
+        box.style.backgroundColor = 'black';
+    })
+})
+
+const buttonClear = document.querySelector('.clear');
+
+function clear() {
+    boxes.forEach((box) => {
+        box.style.backgroundColor = 'beige';
+    })
+}
+
+buttonClear.addEventListener("click", () => {
+    clear();
+});

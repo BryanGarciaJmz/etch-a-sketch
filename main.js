611e76ed1dea29cbb33c56c0ca6fba210e8ce903
BryanGarciaJmz <bjgj6318@gmail.com>
2024-04-30 22:34:27 -0600

    Add the change size functionalities, and some CSS

--- main.js
+++ main.js
@@ -1,32 +1,63 @@
 const container = document.querySelector('.container');
+const buttonSize = document.querySelector(".mainButton");
+const buttonClear = document.querySelector('.clear');
 
-function addRows() {
-    for (let i = 0; i <= 15; i++) {
+if (container.hasChildNodes) {
+    changeSize(16);        
+}
+
+
+buttonSize.addEventListener("click", () => {
+    const numberBox = prompt("Insert the number of squares of the grid. MAX = 100; MIN = 16;")
+    
+    if (numberBox > 100) {
+        alert("The maximum number of rows is 100");
+        return
+    } else if (numberBox < 16) {
+        alert("The number must be greater than 16")
+    } else {
+        remove();
+        changeSize(numberBox);
+    }
+});
+
+function changeSize(numberBox) {
+
+    if (!container.childNodes) {
+        changeSize(16);        
+    }
+
+    for (let i = 0; i <= numberBox; i++) {
         const row = document.createElement('div');
         row.classList.add('row');
         container.appendChild(row);
         
-        for (let i = 0; i <= 15; i++) {
+        for (let i = 0; i <= numberBox; i++) {
             const grid = document.createElement('div');
             grid.classList.add('box');
             row.appendChild(grid);
         }
     }
+    readyToPrint();
 }
 
-addRows();
+function remove() {
+    container.innerHTML = '';
+}
 
-const boxes = document.querySelectorAll('.box');
 
-boxes.forEach((box) => {
-    box.addEventListener("mouseover", () => {
-        box.style.backgroundColor = 'black';
+function readyToPrint() {
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach((box) => {
+        box.addEventListener("mouseover", () => {
+            box.style.backgroundColor = 'black';
+        })
     })
-})
+}
 
-const buttonClear = document.querySelector('.clear');
 
 function clear() {
+    const boxes = document.querySelectorAll('.box');
     boxes.forEach((box) => {
         box.style.backgroundColor = 'beige';
     })
